3ec49cf6f85bb58d9ef3a6b1a172e0b10f861a3f
Chris LoJacono <clojacono92@gmail.com>
2020-11-26 13:45:09 -0600

    create the player form complete

--- src/components/Views/FullTeam.js
+++ src/components/Views/FullTeam.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { getPlayers } from '../../helpers/data/playerData';
 import PlayerCard from '../Cards/PlayerCard';
+import AppModal from '../AppModal';
+import PlayerForm from '../Forms';
 
 export default class FullTeam extends Component {
   state = {
@@ -25,6 +27,9 @@ export default class FullTeam extends Component {
     return (
         <>
         <h1>Nashville Predators Roster</h1>
+        <AppModal title={'Add Player'} buttonLabel={'Add Player'}>
+            <PlayerForm onUpdate={this.getThePlayers}/>
+        </AppModal>
         <div className="d-flex flex-wrap justify-content-center">
             {players.map((player) => <PlayerCard playerData={player}/>)}
         </div>
